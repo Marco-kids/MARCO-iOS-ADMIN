91c4c4f1227b181e0fc36bca467f9d09bd96af3c
josecas <jmcg0901@gmail.com>
2023-02-16 21:59:05 -0600

    Added Editor for AR Experience

--- ARPersistencySwiftUI/TableViewCells/EditorTableViewCell.swift
+++ ARPersistencySwiftUI/TableViewCells/EditorTableViewCell.swift
@@ -8,16 +8,42 @@
 import UIKit
 
 class EditorTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imgView: UIImageView!
+    var location : ARLocation?
+    
+    func setCell() {
+        self.nameLabel.text = location?.nombre
+        self.imgView.imageFromServerURL(Params.baseURL + location!.screenshot, placeHolder: nil)
     }
+    
+}
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+extension UIImageView {
+    
+    func imageFromServerURL(_ URLString: String, placeHolder: UIImage?) {
+        self.image = nil
+        let imageServerUrl = URLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+        
+        if let url = URL(string: imageServerUrl) {
+            URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                if error != nil {
+                    print("ERROR LOADING IMAGES FROM URL: \(String(describing: error))")
+                    DispatchQueue.main.async {
+                        self.image = placeHolder
+                    }
+                    return
+                }
+                DispatchQueue.main.async {
+                    if let data = data {
+                        if let downloadedImage = UIImage(data: data) {
+                            self.image = downloadedImage
+                        }
+                    }
+                }
+            }).resume()
+        }
     }
-
+    
 }
--- ARPersistencySwiftUI/ViewControllers/EditorViewController.swift
+++ ARPersistencySwiftUI/ViewControllers/EditorViewController.swift
@@ -7,23 +7,60 @@
 
 import UIKit
 
-class EditorViewController: UIViewController {
+protocol EditorViewControllerDelegate: AnyObject {
+    func loadedData(locations: [ARLocation])
+}
 
+class EditorViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, EditorViewControllerDelegate {
+    
+    
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var tableView: UITableView!
+    var locations: [ARLocation] = []
+    let network = Network.shared
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        network.delegateARVC = self
+        resetButton.layer.cornerRadius = resetButton.layer.frame.height / 2
+        resetButton.clipsToBounds = true
+        saveButton.layer.cornerRadius = saveButton.layer.frame.height / 2
+        saveButton.clipsToBounds = true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: TableView delegate methods
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return locations.count
     }
-    */
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: EditorTableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell") as! EditorTableViewCell
+        cell.location = self.locations[indexPath.row]
+        cell.setCell()
+        return cell
+    }
+    
+    // MARK: UIButtons
+    
+    @IBAction func saveButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func didTapAdd(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
+    
+    // MARK: Protocol Methods
+    
+    func loadedData(locations: [ARLocation]) {
+        self.locations = locations
+        self.tableView.reloadData()
+    }
 
 }
