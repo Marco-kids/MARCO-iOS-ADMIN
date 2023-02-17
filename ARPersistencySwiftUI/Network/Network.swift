b320ef5dcc67d8e4480aecbdc1d333551098f50a
josecas <jmcg0901@gmail.com>
2023-02-17 00:16:49 -0600

    Optimized ARWorldMap downloads

--- ARPersistencySwiftUI/Network/Network.swift
+++ ARPersistencySwiftUI/Network/Network.swift
@@ -19,12 +19,12 @@ class Network {
     weak var delegateARVC: EditorViewControllerDelegate?
     
     init() {
-        self.getARWorldMap()
+        self.getLocations()
     }
     
-    func uploadImage(image1: UIImage, arWorldMap: Data) {
+    func uploadLocation(name: String, image1: UIImage, arWorldMap: Data) {
         let url = Params.createLocationURL
-        let parameters: [String: String] = ["nombre": "test"]
+        let parameters: [String: String] = ["nombre": name]
         AF.upload(multipartFormData: { multipartFormData in
             for (key, value) in parameters {
                 multipartFormData.append(value.data(using: .utf8)!, withName: key)
@@ -45,18 +45,19 @@ class Network {
         }
     }
     
-    func getARWorldMap() {
+    func getLocations() {
         let url = Params.locationsURL
         AF.request(url).responseJSON { [self] response in
             switch response.result {
             case .success(let value):
                 do {
-                    print("Success dowloading intial data")
                     let arLocation = try JSONDecoder().decode([ARLocation].self, from: response.data!)
                     self.locations = arLocation
                     guard let delegateEditor = delegateARVC else { return }
                     delegateEditor.loadedData(locations: arLocation)
-                    self.downloadFile(url: self.locations.first!.ARWorldMap)
+                    for item in arLocation {
+                        self.getARWordlMap(location: item)
+                    }
                 } catch {
                     print("Error decoding JSON: \(error)")
                 }
@@ -66,15 +67,42 @@ class Network {
         }
     }
     
-    func downloadFile(url: String) {
+    func getARWordlMap(location: ARLocation) {
+        let fileName = location.ARWorldMap.components(separatedBy: "/").last
+        let fileManager = FileManager.default
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let filePathToSearch = documentsURL.relativePath + "/" + fileName!
+        let filePathToSearchURL = URL(string: "file://" + filePathToSearch)
+        if fileManager.fileExists(atPath: filePathToSearch) {
+            for i in self.locations.indices {
+                if self.locations[i].nombre == location.nombre {
+                    self.locations[i].locationPath = filePathToSearchURL
+                    guard let delegateEditor = delegateARVC else { return }
+                    delegateEditor.loadedData(locations: self.locations)
+                }
+            }
+        } else {
+            downloadARWorldMap(location: location)
+        }
+    }
+    
+    func downloadARWorldMap(location: ARLocation) {
         let destination = DownloadRequest.suggestedDownloadDestination(for: .documentDirectory)
-        AF.download(Params.baseURL + url, to: destination).responseData { response in
+        AF.download(Params.baseURL + location.ARWorldMap, to: destination).responseData { response in
             if let error = response.error {
                 print("Error downloading file: \(error)")
             } else if let data = response.value {
                 self.downloadedData.append(data)
-                print("File downloaded to: \(response.fileURL?.path ?? "")")
+                for i in self.locations.indices {
+                    if self.locations[i].nombre == location.nombre {
+                        print(response.fileURL as Any)
+                        self.locations[i].locationPath = response.fileURL
+                        guard let delegateEditor = self.delegateARVC else { return }
+                        delegateEditor.loadedData(locations: self.locations)
+                    }
+                }
             }
         }
     }
+    
 }
